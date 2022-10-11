845e34adf563ca3789a571cdae65ee19f5e98452
Coşkun AKARSU <cskkrs1@gmail.com>
2022-10-11 20:27:21 +0300

    selamladı

---
+++ src/Programmm.java
@@ -0,0 +1,29 @@
+public class Programmm {
+    public static void main(String[] args){
+        /*System.out.println("umut daima umut!");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+
+         */
+
+//      Task: Yıldızlardan oluşan bir kare çıktısı veren bir java programı yazınız.
+    /*  System.out.print("* * *");
+        System.out.print("* * *");
+        System.out.print("* * *");
+
+     */
+        /*   System.out.print("Selam ");
+        System.out.println("Yoldaş!");*/
+        System.out.println("     *  ");
+        System.out.println("   *   *  ");
+        System.out.println("  *     *");
+        System.out.println(" *       *");
+        System.out.println("***********");
+
+        System.out.println("merhaba iki gözüm");
+
+    }
+
+
+}
